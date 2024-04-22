d9bdb205966da1a79510df52b41ddbd7b570826e
YeThura-424 <rathanpoudle.26@gmail.com>
2024-04-22 21:33:37 +0630

    ui style update

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./components/home";
 import About from "./components/about";
 import Users from "./components/users";
+import NavBar from "./components/navbar";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/users" element={<Users />} />
+        <Route path="/" element={<NavBar />}>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/users" element={<Users />} />
+        </Route>
       </Routes>
     </div>
   );
