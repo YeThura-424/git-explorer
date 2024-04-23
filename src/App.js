a00d8eb9d450d7b0d4ff8d8bef6017c2a27f1097
YeThura-424 <rathanpoudle.26@gmail.com>
2024-04-23 22:55:00 +0630

    route file separated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import { Route, Routes, Navigate, useLocation } from "react-router-dom";
-import Home from "./components/home";
-import About from "./components/about";
-import NotFound from "./components/notFound";
 import { CSSTransition, SwitchTransition } from "react-transition-group";
 import "./App.css";
-import { useState, lazy, Suspense } from "react";
-
-const Users = lazy(() => import("./components/users"));
-const UserProfile = lazy(() => import("./components/userProfile"));
-const SearchUser = lazy(() => import("./components/searchUser"));
-const Login = lazy(() => import("./components/login"));
-const AuthProfile = lazy(() => import("./components/authProfile"));
+import { useState, Suspense } from "react";
+import { appRoutes } from "./routes";
 
 function App() {
   const [username, setUsername] = useState("");
@@ -27,28 +19,33 @@ function App() {
         >
           <Suspense fallback={() => <h1>Loading ...</h1>}>
             <Routes location={location}>
-              <Route exact path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/users" element={<Users />} />
-              <Route path="/users/user/:username" element={<UserProfile />} />
-              <Route path="/search" element={<SearchUser />} />
-              <Route
-                path="/login"
-                element={
-                  <Login setIsLogged={setIsLogged} setUsername={setUsername} />
+              {appRoutes.map((route) => {
+                if (route.requiresAuth && !isLogged) {
+                  return (
+                    <Route
+                      key={route.path}
+                      exact
+                      path={route.path}
+                      element={<Navigate replace to={"/login"} />}
+                    />
+                  );
+                } else {
+                  return (
+                    <Route
+                      key={route.path}
+                      exact
+                      path={route.path}
+                      element={
+                        <route.component
+                          setIsLogged={setIsLogged}
+                          setUsername={setUsername}
+                          username={username}
+                        />
+                      }
+                    />
+                  );
                 }
-              />
-              <Route
-                path="/auth-profile"
-                element={
-                  isLogged ? (
-                    <AuthProfile username={username} />
-                  ) : (
-                    <Navigate replace to="/login" />
-                  )
-                }
-              />
-              <Route path="*" element={<NotFound />} />
+              })}
             </Routes>
           </Suspense>
         </CSSTransition>
---
+++ src/routes.js
@@ -0,0 +1,53 @@
+import { lazy } from "react";
+import Home from "./components/home";
+
+const Users = lazy(() => import("./components/users"));
+const UserProfile = lazy(() => import("./components/userProfile"));
+const SearchUser = lazy(() => import("./components/searchUser"));
+const Login = lazy(() => import("./components/login"));
+const AuthProfile = lazy(() => import("./components/authProfile"));
+const About = lazy(() => import("./components/about"));
+const NotFound = lazy(() => import("./components/notFound"));
+
+export const appRoutes = [
+  {
+    path: "/",
+    component: Home,
+    requiresAuth: false,
+  },
+  {
+    path: "/users",
+    component: Users,
+    requiresAuth: false,
+  },
+  {
+    path: "/users/user/:username",
+    component: UserProfile,
+    requiresAuth: false,
+  },
+  {
+    path: "/search",
+    component: SearchUser,
+    requiresAuth: false,
+  },
+  {
+    path: "/login",
+    component: Login,
+    requiresAuth: false,
+  },
+  {
+    path: "/auth-profile",
+    component: AuthProfile,
+    requiresAuth: true,
+  },
+  {
+    path: "/about",
+    component: About,
+    requiresAuth: false,
+  },
+  {
+    path: "*",
+    component: NotFound,
+    requiresAuth: false,
+  },
+];
